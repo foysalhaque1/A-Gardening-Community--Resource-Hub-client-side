9426d8d4480e13a1a742328d481d0befd8ed4485
user <foysalhaque.mist.ipe@gmail.com>
2025-05-22 10:29:22 +0600

    update browse tip

--- src/Components/BrowseTip.jsx
+++ src/Components/BrowseTip.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router';
 import BrowseTipTable from './BrowseTipTable';
+import Swal from 'sweetalert2';
 
 const BrowseTip = () => {
     const data = useLoaderData();
     console.log(data)
-    const publicTips = data.filter(pubData => pubData.condition == "Public")
-    console.log(publicTips)
+    const initialPublicTips = data.filter(pubData => pubData.condition == "Public")
+    const [publicTips,setPublicTips] = useState(initialPublicTips)
+    console.log(publicTips);
+    const handleDelete = (id) => {
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:4000/tips/${id}`,{
+                    method:'DELETE',
+                  
+                }).then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    const remainingUsers = publicTips.filter(user=>user._id !== id);
+                    setPublicTips(remainingUsers)
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                })
+            }
+        });
+    }
     return (
         <div>
 
-          <div className="overflow-x-auto">
+            <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
                     <thead>
@@ -19,9 +50,10 @@ const BrowseTip = () => {
                                 No
                             </th>
                             <th>Image</th>
-                            <th>Phone no</th>
-                            <th>Email</th>
-                            <th></th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+
                         </tr>
                     </thead>
                     <tbody>
@@ -40,25 +72,21 @@ const BrowseTip = () => {
                                                     alt="Avatar Tailwind CSS Component" />
                                             </div>
                                         </div>
-                                        
+
                                     </div>
                                 </td>
-                                <td>s
+                                <td>
                                     {user.Title}
                                 </td>
                                 <td>{user.Category}</td>
+                                <td>{user.condition}</td>
                                 <th className='space-x-3'>
                                     <button className="btn  btn-xs">View Details</button>
                                     <button className="btn  btn-xs">Edit</button>
-                                    {/* <button onClick={() => handleDelete(user._id)} className="btn  btn-xs">X</button> */}
+                                    <button onClick={() => handleDelete(user._id)} className="btn  btn-xs">Delete</button>
                                 </th>
                             </tr>)
                         }
-                        {/* row 2 */}
-
-                        {/* row 3 */}
-
-                        {/* row 4 */}
 
                     </tbody>
                     {/* foot */}
